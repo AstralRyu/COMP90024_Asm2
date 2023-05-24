371ee7d8104b9f3d85266baba03f42bd0b968bb2
Jiaxin Liu <steve@Jiaxins-MacBook-Air.local>
2023-05-24 18:10:22 +1000

    modify grafana initialization file

--- grafana/grafana_start.sh
+++ grafana/grafana_start.sh
@@ -1,5 +1,4 @@
 docker run -d --name=grafana -p 3000:3000 \
 --restart=always \
--v /home/ubuntu/grafana/grafana.ini:/etc/grafana/grafana.ini \
 -e "GF_PANELS_DISABLE_SANITIZE_HTML=true" \
 grafana/grafana
